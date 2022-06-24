587ff2ce24077f1df3d9c2aca314b9a981e9eccb
ParvesPR <parves2585@gmail.com>
2022-06-24 13:16:22 +0600

    slider images added

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import PrayerTime from '../PrayerTime/PrayerTime';
 
 const Home = () => {
     return (
         <div>
            <Banner></Banner>
+           <PrayerTime></PrayerTime>
         </div>
     );
 };
---
+++ src/pages/Home/PrayerTime/PrayerTime.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import prayerBanner from '../../../assests/images/prayer-banner.jpg'
+
+const PrayerTime = () => {
+    return (
+        <section>
+            <div></div>
+            <div>
+            </div>
+        </section>
+    );
+};
+
+export default PrayerTime;
