e601ab314315137e2c4e2358082bff53b7dd6972
ParvesPR <parves2585@gmail.com>
2022-06-25 09:55:52 +0600

    committe image added

---
+++ src/pages/Home/Committee/Committee.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import underline from '../../../assests/images/underline.png';
+import image1 from '../../../assests/images/image1.png'
+
+const Committee = () => {
+    return (
+        <section className='mt-6 px-12'>
+            <h2 className='text-center uppercase font-bold text-[#FFA90F] text-4xl'>Masjid Committee</h2>
+            <div className='text-center block w-2/4 mx-auto'>
+                <img src={underline} className='w-6/12 mx-auto' alt="" />
+            </div>
+            <div className='grid gap-5 grid-cols-4'>
+                <div> <img src={image1} className='w-6/12 rounded-full border-solid' alt="" /></div>
+            </div>
+        </section>
+    );
+};
+
+export default Committee;
