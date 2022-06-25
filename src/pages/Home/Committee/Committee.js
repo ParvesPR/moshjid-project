72f770d06dd531d2db62512fa524a01757776391
ParvesPR <parves2585@gmail.com>
2022-06-25 10:34:07 +0600

    committee images added

--- src/pages/Home/Committee/Committee.js
+++ src/pages/Home/Committee/Committee.js
@@ -9,6 +9,13 @@ const Committee = () => {
             <div className='text-center block w-2/4 mx-auto'>
                 <img src={underline} className='w-6/12 mx-auto' alt="" />
             </div>
+
+            <div className='flex justify-center gap-7'>
+                <img src={image1} className='w-[280px] rounded-full border-[#FFA90F] border-8' alt="" />
+                <img src={image1} className='w-[280px] rounded-full border-[#FFA90F] border-8' alt="" />
+                <img src={image1} className='w-[280px] rounded-full border-[#FFA90F] border-8' alt="" />
+                <img src={image1} className='w-[280px] rounded-full border-[#FFA90F] border-8' alt="" />
+            </div>
         </section>
     );
 };
