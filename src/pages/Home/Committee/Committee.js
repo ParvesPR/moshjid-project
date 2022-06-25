4f310c7442256d1daba83575c866b599c9b02293
ParvesPR <parves2585@gmail.com>
2022-06-25 10:05:45 +0600

    committe image added

--- src/pages/Home/Committee/Committee.js
+++ src/pages/Home/Committee/Committee.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import underline from '../../../assests/images/underline.png';
-<<<<<<< HEAD
+import image1 from '../../../assests/images/image1.png';
 
 const Committee = () => {
     return (
         <section className='mt-6'>
-=======
-import image1 from '../../../assests/images/image1.png'
-
-const Committee = () => {
-    return (
-        <section className='mt-6 px-12'>
->>>>>>> parves
             <h2 className='text-center uppercase font-bold text-[#FFA90F] text-4xl'>Masjid Committee</h2>
             <div className='text-center block w-2/4 mx-auto'>
                 <img src={underline} className='w-6/12 mx-auto' alt="" />
+            </div>
+
+            <div className='flex justify-center gap-5'>
+                <img src={image1} className='w-[285px] rounded-full border-8' alt="" />
+                <img src={image1} className='w-[285px] rounded-full border-8' alt="" />
+                <img src={image1} className='w-[285px] rounded-full border-8' alt="" />
+                <img src={image1} className='w-[285px] rounded-full border-8' alt="" />
             </div>
         </section>
     );
