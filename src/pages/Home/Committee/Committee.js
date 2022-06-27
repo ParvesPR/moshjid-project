c4f3960e1dbed96c28a322e12240129436f9cba5
ParvesPR <parves2585@gmail.com>
2022-06-27 20:15:31 +0600

    responsive mashjid committee

--- src/pages/Home/Committee/Committee.js
+++ src/pages/Home/Committee/Committee.js
@@ -7,11 +7,11 @@ const Committee = () => {
     return (
         <section className='mt-6 committee-section py-3'>
             <h2 className='text-center uppercase font-bold text-[#FFA90F] text-4xl'>Masjid Committee</h2>
-            <div className='text-center block w-2/4 mx-auto'>
-                <img src={underline} className='w-6/12 mx-auto' alt="" />
+            <div className='text-center block w-2/4 mx-auto mb-9'>
+                <img src={underline} className='sm:max-w-sm lg:w-1/2 mx-auto' alt="" />
             </div>
 
-            <div className='flex justify-center gap-7 my-7'>
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 justify-items-center'>
                 <img src={image1} className='w-[280px] rounded-full border-[#FFA90F] border-8' alt="" />
                 <img src={image1} className='w-[280px] rounded-full border-[#FFA90F] border-8' alt="" />
                 <img src={image1} className='w-[280px] rounded-full border-[#FFA90F] border-8' alt="" />
