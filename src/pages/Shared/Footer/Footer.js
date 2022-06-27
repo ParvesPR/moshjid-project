b592124a2395475f0aad0b1acf45633e4ba26d52
ParvesPR <parves2585@gmail.com>
2022-06-27 20:05:16 +0600

    footer font using bangla

--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -2,22 +2,24 @@ import React from 'react';
 import './Footer.css';
 import { GoLocation } from 'react-icons/go';
 import { FiPhoneCall } from 'react-icons/fi';
-import underline from '../../../assests/images/underline.png'
+import underlineFooter from '../../../assests/images/underline-footer.png'
 
 const Footer = () => {
     return (
-        <footer className='footer-section flex justify-around py-10 mt-7'>
-            <div className='text-white'>
-                <span className='flex gap-2 items-center'><GoLocation></GoLocation> Station Road, Bogura</span>
-                <span className='flex gap-2 items-center'> <FiPhoneCall></FiPhoneCall> 01789445555</span>
+        <footer className='footer-section mt-7'>
+            <div className='text-white text-2xl lg:text-3xl sm:flex justify-center gap-10 align-middle pt-28 px-[107px]'>
+            <div className='mb-7'>
+                <span className='flex mb-1 lg:mb-5 gap-2 items-center'><GoLocation></GoLocation>স্টেশন রোড, বগুড়া সদর, বগুড়া-৫৮০০ </span>
+                <span className='flex gap-2 items-center'> <FiPhoneCall></FiPhoneCall>+৮৮০১৭৪৫৩৩৩৬২৪, +৮৮০১৫১৬৭৪৭৩৭৭ </span>
             </div>
-            <div className='footer-image'>
-                <img src={underline} className='w-40 images' alt="" />
+            <div>
+                <img src={underlineFooter} className='h-44 hidden lg:block' alt="" />
             </div>
-            <div className='text-white'>
-                <h3>For Donation</h3>
-                <span className='flex gap-2 items-center'><GoLocation></GoLocation> Station Road, Bogura</span>
-                <span className='flex gap-2 items-center'> <FiPhoneCall></FiPhoneCall> 01789445555</span>
+            <div>
+                <h3 className='mb-1 lg:mb-5'>মুক্ত হস্তে দান করুন</h3>
+                <span className='flex gap-2 mb-1 items-center'><GoLocation></GoLocation> +৮৮০১৭৪৫৩৩৩৬২৪, (বিকাশ, নগদ, রকেট) </span>
+                <span className='flex gap-2 items-center'> <FiPhoneCall></FiPhoneCall> +৮৮০১৫১৬৭৪৭৩৭৭</span>
+            </div>
             </div>
         </footer>
     );
