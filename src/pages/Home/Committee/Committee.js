3cf228e1ec374d84c24fc504164252cf2ddd71c7
ParvesPR <parves2585@gmail.com>
2022-06-28 12:43:31 +0600

    responsive footer

--- src/pages/Home/Committee/Committee.js
+++ src/pages/Home/Committee/Committee.js
@@ -11,10 +11,10 @@ const Committee = () => {
                 <img src={underline} className='sm:max-w-sm lg:w-1/2 mx-auto' alt="" />
             </div>
 
-            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 justify-items-center'>
-                <img src={image1} className='w-[280px] rounded-full border-[#FFA90F] border-8' alt="" />
-                <img src={image1} className='w-[280px] rounded-full border-[#FFA90F] border-8' alt="" />
-                <img src={image1} className='w-[280px] rounded-full border-[#FFA90F] border-8' alt="" />
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 justify-items-center'>
+                <img src={image1} className='w-[280px] mb-2 rounded-full border-[#FFA90F] border-8' alt="" />
+                <img src={image1} className='w-[280px] mb-2 rounded-full border-[#FFA90F] border-8' alt="" />
+                <img src={image1} className='w-[280px] mb-2 rounded-full border-[#FFA90F] border-8' alt="" />
                 <img src={image1} className='w-[280px] rounded-full border-[#FFA90F] border-8' alt="" />
             </div>
         </section>
