643acf4a99bd0e7e934fdd24dfcf9127dac2c973
ParvesPR <parves2585@gmail.com>
2022-06-30 12:10:09 +0600

    blogs route added in app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from '../src/pages/Home/Home/Home';
+import Blogs from './pages/Home/Blogs/Blogs';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>} ></Route>
         <Route path='/home' element={<Home></Home>} ></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>} ></Route>
       </Routes>
 
     </div>
