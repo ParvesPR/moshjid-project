b022eb257e2b45c302ee28b1f5c7ad6b25b90186
ParvesPR <parves2585@gmail.com>
2022-07-01 12:46:39 +0600

    updated file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from '../src/pages/Home/Home/Home';
+import Login from './pages/Home/Login/Login';
+import SignUp from './pages/Home/Login/SignUp';
+import Error404 from './pages/Shared/Error404';
+import NavBar from './pages/Shared/NavBar/NavBar';
+import Blogs from './pages/Home/Blogs/Blogs';
 
 function App() {
   return (
     <div>
+      <NavBar/>
       <Routes>
         <Route path='/' element={<Home></Home>} ></Route>
         <Route path='/home' element={<Home></Home>} ></Route>
+        <Route path='/login' element={<Login/>} ></Route>
+        <Route path='/signup' element={<SignUp/>}></Route>
+        <Route path='*' element={<Error404/>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>} ></Route>
       </Routes>
 
     </div>
