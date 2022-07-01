77e21815f87650463ec0df7a2f7b5b0090cd0be7
ParvesPR <parves2585@gmail.com>
2022-07-01 12:57:19 +0600

    fix code conflicts

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h2 className='text-5xl font-bold'>About us</h2>
+            <h2 className='text-5xl font-bold bg-slate-500'>About us</h2>
         </div>
     );
 };
