e3540735226c74318c37e0b864c46f883baea578
ParvesPR <parves2585@gmail.com>
2022-07-05 12:24:18 +0600

    about us picture added

--- src/pages/Home/PrayerTime/PrayerTime.js
+++ src/pages/Home/PrayerTime/PrayerTime.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './PrayerTime.css';
+import about from '../../../assests/images/about.jpg'
+import aboutBorder from '../../../assests/images/about-border.png'
 
 const PrayerTime = () => {
     return (
-        <section className="prayer-section pt-12 pb-7 px-12 border-b-4 border-t-4 border-[#c4c4c4]">
-            <div className="cardBg card w-96 bg-[#e9e9e9] shadow-lg">
+        <section className="prayer-section pt-12 pb-7 px-12 border-b-4 border-t-4 border-[#c4c4c4] flex justify-center gap-20">
+            <div className="cardBg card w-96 bg-[#e9e9e9] shadow-lg mb-3">
                 <div className="card-body text-3xl font-bold">
                     <div className='grid lg:grid-cols-3 border-b-2 border-[#c4c4c4] justify-items-center pb-2'>
                         <p>ফজর </p>
@@ -53,6 +55,15 @@ const PrayerTime = () => {
                     </div>
                 </div>
             </div>
+
+            <article>
+                <div className='grid grid-cols-2 gap-3'>
+                    <div className='relative'>
+                        <img src={about} className='relative rounded-lg w-80 lg:h-[304px] z-[11]' alt="" />
+                        <img src={aboutBorder} className='absolute -bottom-7 -left-7 z-[1] hidden lg:block' alt="" />
+                    </div>
+                </div>
+            </article>
         </section>
     );
 };
