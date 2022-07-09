217ad05cc24dd8e932249d46f7c3cc2e43e0a5f9
ParvesPR <parves2585@gmail.com>
2022-07-09 13:17:23 +0800

    dashboard drawer bg img added

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,26 +1,33 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import { Outlet } from "react-router-dom";
+import NavBar from '../Shared/NavBar/NavBar';
+import './Dashboard.css';
 
 const Dashboard = () => {
     return (
-        <div class="drawer drawer-mobile">
-            <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-            <div class="drawer-content flex flex-col items-center justify-center">
-                <Outlet />
-                <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-                {/* <!-- Page content here --> */}
+        <section>
+            <div className='my-10'>
+                <NavBar></NavBar>
             </div>
-            <div class="drawer-side">
-                <label for="my-drawer-2" class="drawer-overlay"></label>
-                <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-                    {/* <!-- Sidebar content here --> */}
-                    <li><a>Sidebar Item 1</a></li>
-                    <li><a>Sidebar Item 2</a></li>
-                </ul>
+            <div class="drawer drawer-mobile bg-slate-400">
+                <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+                <div class="drawer-content flex flex-col items-center justify-center">
+                    <Outlet />
+                    <h1 className='uppercase font-bold text-3xl text-center'>Welcome To Dashboard</h1>
+                    <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+                   
+                </div>
+                <div class="drawer-side">
+                    <label for="my-drawer-2" class="drawer-overlay"></label>
+                    <ul class="side-bg menu p-4 overflow-y-auto w-80 text-white">
+                        <li><a>Sidebar Item 1</a></li>
+                        <li><a>Sidebar Item 2</a></li>
+                    </ul>
 
+                </div>
             </div>
-        </div>
+        </section>
     );
 };
 
