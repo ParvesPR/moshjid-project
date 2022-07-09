64f572b407f9ef3dade7049dda8747e7727f9e81
ParvesPR <parves2585@gmail.com>
2022-07-10 00:04:14 +0800

    dashboard drawer name set

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,28 +1,30 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import NavBar from '../Shared/NavBar/NavBar';
 import './Dashboard.css';
 
 const Dashboard = () => {
     return (
-        <section>
-            <div className='my-10'>
+        <section className='bg-base-200'>
+            <div className='pt-7 pb-2'>
                 <NavBar></NavBar>
             </div>
-            <div class="drawer drawer-mobile bg-slate-400">
+            <div class="drawer drawer-mobile bg-base-200">
                 <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-                <div class="drawer-content flex flex-col items-center justify-center">
+                <div class="drawer-content">
                     <Outlet />
-                    <h1 className='uppercase font-bold text-3xl text-center'>Welcome To Dashboard</h1>
-                    <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-                   
+                    <h1 className='uppercase font-bold text-3xl text-center py-10'>Welcome To Dashboard</h1>
+
                 </div>
                 <div class="drawer-side">
                     <label for="my-drawer-2" class="drawer-overlay"></label>
-                    <ul class="side-bg menu p-4 overflow-y-auto w-80 text-white">
-                        <li><a>Sidebar Item 1</a></li>
-                        <li><a>Sidebar Item 2</a></li>
+                    <ul class="custom-font side-bg menu px-4 py-10 overflow-y-auto w-80 text-white">
+                        <NavLink className='mt-2 font-normal text-white btn btn-outline' to="/dashboard/users">আমার প্রোফাইল</NavLink>
+                        <NavLink className='mt-2 font-normal text-white btn btn-outline' to="/dashboard/users">সকল ব্যবহারকারী</NavLink>
+                        <NavLink className='mt-2 font-normal text-white btn btn-outline' to="/dashboard/users">নতুন নোটিশ পোস্ট করুন</NavLink>
+                        <NavLink className='mt-2 font-normal text-white btn btn-outline' to="/dashboard/users">নতুন ব্লগ যোগ করুন</NavLink>
+                        <NavLink className='mt-2 font-normal text-white btn btn-outline' to="/dashboard/users">নামাজের সময়সূচী পরিবর্তন করুন</NavLink>
                     </ul>
 
                 </div>
--- src/pages/Shared/Error404.js
+++ src/pages/Shared/Error404.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import notFound from '../../assests/images/404.gif';
 
 const Error404 = () => {
     return (
         <div>
-            <img src={notFound} alt="" />
+            <h3 className='text-center font-bold text-3xl text-red-600 py-7'>Page not found</h3>
+            <Link className='font-bold text-blue-600 p-10 underline' to="/home">Back To home</Link>
+            <img className='flex justify-center lg:max-w-2/4 mx-auto' src={notFound} alt="" />
         </div>
     );
 };
