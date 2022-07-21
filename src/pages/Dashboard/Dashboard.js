ad6454ba2cf1a521929129dad4f77605f1ecb0bc
ParvesPR <parves2585@gmail.com>
2022-07-21 14:19:27 +0800

    conditionaly display dashboard route

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,10 +1,15 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { NavLink, Outlet } from "react-router-dom";
+import auth from '../../firebase.init';
+import useAdmin from '../../hooks/useAdmin';
 import NavBar from '../Shared/NavBar/NavBar';
 import './Dashboard.css';
 
 const Dashboard = () => {
+    const [user] = useAuthState(auth);
+    const [admin] = useAdmin(user);
     return (
         <section className='bg-base-200'>
             <div className='pt-7 pb-2'>
@@ -21,10 +26,13 @@ const Dashboard = () => {
                     <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
                     <ul className="custom-font side-bg menu px-4 py-10 overflow-y-auto w-80 text-white">
                         <NavLink className='mt-2 font-normal text-white btn btn-outline' to="/dashboard/users">আমার প্রোফাইল</NavLink>
-                        <NavLink className='mt-2 font-normal text-white btn btn-outline' to="/dashboard/allUsers">সকল ইউজারস</NavLink>
-                        <NavLink className='mt-2 font-normal text-white btn btn-outline' to="/dashboard/addNotice">নতুন নোটিশ যোগ করুন</NavLink>
-                        <NavLink className='mt-2 font-normal text-white btn btn-outline' to="/dashboard/users">নতুন ব্লগ যোগ করুন</NavLink>
-                        <NavLink className='mt-2 font-normal text-white btn btn-outline' to="/dashboard/users">নামাজের সময়সূচী পরিবর্তন করুন</NavLink>
+                        {admin && <>
+                            <NavLink className='mt-2 font-normal text-white btn btn-outline' to="/dashboard/allUsers">সকল ইউজারস</NavLink>
+                            <NavLink className='mt-2 font-normal text-white btn btn-outline' to="/dashboard/addNotice">নতুন নোটিশ যোগ করুন</NavLink>
+                            <NavLink className='mt-2 font-normal text-white btn btn-outline' to="/dashboard/users">নতুন ব্লগ যোগ করুন</NavLink>
+                            <NavLink className='mt-2 font-normal text-white btn btn-outline' to="/dashboard/users">নামাজের সময়সূচী পরিবর্তন করুন</NavLink> </>
+                        }
+
                     </ul>
 
                 </div>
