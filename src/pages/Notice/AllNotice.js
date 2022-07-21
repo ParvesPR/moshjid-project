78d6c717d8f63fe4a03caf38aea6e6a0211a309b
ParvesPR <parves2585@gmail.com>
2022-07-22 00:27:31 +0800

    show delete icon conditionaly admin user

--- src/pages/Notice/AllNotice.js
+++ src/pages/Notice/AllNotice.js
@@ -1,7 +1,13 @@
 import React, { useState } from 'react';
-import './AllNotice.css';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { RiDeleteBin6Line } from "react-icons/ri";
+import useAdmin from '../../hooks/useAdmin';
+import auth from '../../firebase.init';
 
 const AllNotice = ({ notice }) => {
+    const [user] = useAuthState(auth);
+    const [admin] = useAdmin(user);
+
     const [showMore, setShowMore] = useState(true);
     const { name, subject } = notice;
 
@@ -9,7 +15,11 @@ const AllNotice = ({ notice }) => {
         <div className='px-12'>
             <div className="card bg-base-100 shadow-xl mb-3 content h-min">
                 <div className="card-body">
-                    <h2 className="card-title">{name}</h2>
+                    <div className='flex items-center gap-5 justify-between'>
+                        <h2 className="card-title">{name}</h2>
+                        {admin && <span className='border-red-600 rounded-full border-2 p-1 cursor-pointer'><RiDeleteBin6Line /></span>}
+
+                    </div>
 
                     <h5>
                         {showMore ? subject : subject.slice(0, 100)}
