12b4173e146132c66a9edc827b4b15ae0cb170b1
ParvesPR <parves2585@gmail.com>
2022-07-26 13:46:50 +0800

    implement delete modal & succesfully delete a blog

--- src/pages/Home/Blogs/Blog.js
+++ src/pages/Home/Blogs/Blog.js
@@ -1,13 +1,49 @@
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { RiDeleteBin6Line } from 'react-icons/ri';
+import Swal from 'sweetalert2';
 import auth from '../../../firebase.init';
 import useAdmin from '../../../hooks/useAdmin.js';
 
 const Blog = ({ blogs, refetch }) => {
     const [user] = useAuthState(auth)
     const [admin] = useAdmin(user)
-    const { title, image, subject } = blogs;
+    const { title, image, subject, _id } = blogs;
+
+    const deleteConfirm = (id) => {
+        Swal.fire({
+            title: `আপনি কি এই (${title}) ব্লগটি delete করতে চান?`,
+            text: "delete করলে আপনি এটি ফিরিয়ে আনতে পারবেন না!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.value) {
+                const url = `http://localhost:5000/blogs/${id}`;
+                console.log(url);
+                fetch(url, {
+                    method: 'DELETE',
+                    headers: {
+                        'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+                    }
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log('data', data);
+                        refetch();
+                    })
+            }
+            if (result.isConfirmed) {
+                Swal.fire(
+                    'Deleted!',
+                    'সফলভাবে delete করা হয়েছে!',
+                    'success'
+                )
+            }
+        })
+    }
     return (
         <div className="card lg:max-w-lg glass">
             <figure>
@@ -18,10 +54,10 @@ const Blog = ({ blogs, refetch }) => {
                 <p>{subject}</p>
             </div>
             {admin &&
-                <div class="card-actions justify-center p-5">
-                    <div class="ease-in duration-300 btn btn-outline btn-xs sm:btn-sm md:btn-md lg:btn-md flex items-center gap-2 hover:bg-red-800">
+                <div className="card-actions justify-center p-5">
+                    <button onClick={() => deleteConfirm(_id)} className="ease-in duration-300 btn btn-outline btn-xs sm:btn-sm md:btn-sm flex items-center gap-2 hover:bg-red-800">
                         <RiDeleteBin6Line />
-                        Delete</div>
+                        Delete</button>
                 </div>}
         </div>
     );
