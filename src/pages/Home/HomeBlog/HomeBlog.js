b5227f2b6ea2324fed31a4498248616b4cdaa673
ParvesPR <parves2585@gmail.com>
2022-07-28 23:57:01 +0800

    see more btn added in home page blog section

--- src/pages/Home/HomeBlog/HomeBlog.js
+++ src/pages/Home/HomeBlog/HomeBlog.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Blog from './Blog';
 import './HomeBlog.css';
 
@@ -10,13 +11,19 @@ const HomeBlog = () => {
             .then(data => setBlogs(data))
     }, [])
     return (
-        <section className='p-12 blog-section'>
-            {
-                blogs.slice(-3).map(blog => <Blog
-                    key={blog._id}
-                    blog={blog}
-                ></Blog>)
-            }
+        <section>
+            <div className='px-12 mt-12 blog-section'>
+                {
+                    blogs.slice(-3).map(blog => <Blog
+                        key={blog._id}
+                        blog={blog}
+                    ></Blog>)
+                }
+            </div>
+            <div className='flex justify-center py-5'>
+                <button className='btn-sm rounded-md btn-warning font-normal text-lg text-white font-bensen'><Link to='/blogs'>আরও দেখুন</Link></button>
+            </div>
+
         </section>
     );
 };
