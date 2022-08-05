893d366bb1281684e2a50d71cf75b49ff00c4fc2
ParvesPR <parves2585@gmail.com>
2022-08-05 23:54:30 +0800

    about us content added

--- src/pages/Home/PrayerTime/PrayerTime.js
+++ src/pages/Home/PrayerTime/PrayerTime.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './PrayerTime.css';
 import SinglePrayer from '../PrayerTime/SinglePrayer';
-import about from '../../../assests/images/about.jpg';
-import aboutBorder from '../../../assests/images/about-border.png';
+import about2 from '../../../assests/images/about2.png';
+import underline from '../../../assests/images/underline.png';
 import { useQuery } from 'react-query';
 import Loading from '../../Shared/Loading';
 
@@ -17,8 +17,8 @@ const PrayerTime = () => {
         return <Loading></Loading>
     };
     return (
-        <section className="prayer-section pt-12 pb-7 px-12 border-b-2 border-t-4 border-[#c4c4c4] flex flex-col lg:flex-row justify-center gap-20">
-            <div className='card-child'>
+        <section className="prayer-section pt-12 pb-7 px-12 border-b-2 border-t-2 border-[#c4c4c4] flex flex-col lg:flex-row justify-center gap-5">
+            <div className='pl-0 lg:pl-12'>
                 {
                     prayerTime.map(result => <SinglePrayer
                         key={result._id}
@@ -28,16 +28,29 @@ const PrayerTime = () => {
                 }
             </div>
             <article>
-                <div className='grid grid-cols-2 gap-2 justify-center items-center'>
-                    <div className='relative'>
-                        <img src={about} className='relative rounded-lg w-72 lg:h-[290px] z-[11]' alt="" />
-                        <img src={aboutBorder} className='absolute -bottom-7 -left-7 z-[1] hidden lg:block' alt="" />
+                {/* <div className='flex justify-center items-center'>
+                    <img className='w-full' src={about2} alt="" />
+                    <div className='mt-10 text-4xl lg:text-5xl text-[#FFA810] font-bold text-center'>
+                        <p>আমাদের</p>
+                        <p>সম্পর্কে</p>
+                        <p>
+                            <img src={underline} className='lg:w-5/12 mx-auto' alt="" />
+                        </p>
                     </div>
-                    <div>
-                        <p className='text-6xl text-[#FFA810] font-bold text-center'>আমাদের</p>
-                        <p className='text-6xl text-[#FFA810] font-bold text-center'>সম্পর্কে</p>
+                </div> */}
+                <div class="hero items-start justify-center">
+                    <div class="hero-content flex-row lg:flex-row">
+                        <img src={about2} class="max-w-md w-7/12 lg:w-full" alt='about' />
+                        <div>
+                            <h1 class="text-4xl lg:text-6xl text-[#FFA810] font-bold text-center">আমাদের</h1>
+                            <h1 class="text-4xl lg:text-6xl text-[#FFA810] font-bold text-center">সম্পর্কে</h1>
+                            <img src={underline} className='w-3/4 lg:w-8/12 mx-auto' alt="" />
+                        </div>
                     </div>
                 </div>
+            <p className='block font-bensen text-xl lg:text-3xl lg:px-8'>
+                মসজিদের উৎকর্ষের ক্ষেত্রে,সেই সপ্তম শতাব্দীর সাদাসিধে খোলা প্রাঙ্গণবিশিষ্ট মসজিদে কাবা বা মসজিদে নববী থেকে বর্তমানে এর প্রভূত উন্নয়ন ঘটেছে। এখন অনেক মসজিদেরই সুবিশাল গম্বুজ,উচু মিনার এবং বৃহদাকার প্রাঙ্গণ দেখা যায়। মসজিদের উৎপত্তি আরব উপদ্বীপে হলেও বর্তমানে তা পৃথিবীর সব দেশেই ছড়িয়ে পড়েছে।
+            </p>
             </article>
         </section>
     );
--- src/pages/Home/PrayerTime/SinglePrayer.js
+++ src/pages/Home/PrayerTime/SinglePrayer.js
@@ -4,7 +4,7 @@ const SinglePrayer = ({ result, refetch }) => {
     const { name, azan, ekamot } = result;
     return (
         <div className="lg:w-96 sm:w-60 bg-[#e9e9e9] shadow-lg px-5">
-            <div className="px-3 text-2xl font-bold grid grid-cols-3 border-[#C2C2C2] border-b-2 py-2">
+            <div className="px-3 text-2xl font-bold grid grid-cols-3 border-[#C2C2C2] border-b-[1px] py-3">
                 <p>{name}</p>
                 <p className='font-normal'>{azan}</p>
                 <p className='font-normal'>{ekamot}</p>
