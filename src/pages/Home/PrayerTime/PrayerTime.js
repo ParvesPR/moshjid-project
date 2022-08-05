b4ade80279b80184a49e0e86ef53f604c02d17f1
ParvesPR <parves2585@gmail.com>
2022-08-05 13:12:39 +0800

    prayer time chart added

--- src/pages/Home/PrayerTime/PrayerTime.js
+++ src/pages/Home/PrayerTime/PrayerTime.js
@@ -1,65 +1,32 @@
 import React from 'react';
 import './PrayerTime.css';
+import SinglePrayer from '../PrayerTime/SinglePrayer';
 import about from '../../../assests/images/about.jpg';
 import aboutBorder from '../../../assests/images/about-border.png';
+import { useQuery } from 'react-query';
+import Loading from '../../Shared/Loading';
 
 const PrayerTime = () => {
+    const { data: prayerTime, isLoading, refetch } = useQuery('prayer', () =>
+        fetch('http://localhost:5000/prayerTime', {
+            method: 'GET'
+        })
+            .then(res => res.json())
+    );
+    if (isLoading) {
+        return <Loading></Loading>
+    };
     return (
-        <section className="relative prayer-section pt-12 pb-7 px-12 border-b-4 border-t-4 border-[#c4c4c4] flex flex-col lg:flex-row justify-center gap-20">
-              <div className='flex absolute -top-10'>
-                    <span className='bg-[#FFA90F] p-3 mx-3'>আযান</span>
-                    <span className='bg-[#FFA90F] p-3 ml-6'>ইকামত</span>
-                </div>
-            <div className="cardBg card lg:w-96 bg-[#e9e9e9] shadow-lg mb-3">
-                <div className="card-body text-3xl font-bold">
-                    <div className='grid lg:grid-cols-3 border-b-2 border-[#c4c4c4] justify-items-center pb-2'>
-                        <p>ফজর </p>
-                        <p className='font-normal'>৪ঃ৩০</p>
-                        <p className='font-normal'>৫ঃ১০</p>
-                    </div>
-                    <div className='grid lg:grid-cols-3 border-b-2 border-[#c4c4c4] justify-items-center pb-2'>
-                        <p>যোহর </p>
-                        <p className='font-normal'>৪ঃ৩০</p>
-                        <p className='font-normal'>৫ঃ১০</p>
-                    </div>
-                    <div className='grid lg:grid-cols-3 border-b-2 border-[#c4c4c4] justify-items-center pb-2'>
-                        <p>আছর </p>
-                        <p className='font-normal'>৪ঃ৩০</p>
-                        <p className='font-normal'>৫ঃ১০</p>
-                    </div>
-                    <div className='grid lg:grid-cols-3 border-b-2 border-[#c4c4c4] justify-items-center pb-2'>
-                        <p>মাগরিব </p>
-                        <p className='font-normal'>৪ঃ৩০</p>
-                        <p className='font-normal'>৫ঃ১০</p>
-                    </div>
-                    <div className='grid lg:grid-cols-3 border-b-2 border-[#c4c4c4] justify-items-center pb-2'>
-                        <p>এশা </p>
-                        <p className='font-normal'>৪ঃ৩০</p>
-                        <p className='font-normal'>৫ঃ১০</p>
-                    </div>
-                    <div className='grid lg:grid-cols-3 border-b-2 border-[#c4c4c4] justify-items-center pb-2'>
-                        <p>জুম্মা </p>
-                        <p className='font-normal'>৪ঃ৩০</p>
-                        <p className='font-normal'>৫ঃ১০</p>
-                    </div>
-                    <div className='grid lg:grid-cols-3 border-b-2 border-[#c4c4c4] justify-items-center pb-2'>
-                        <p>ইশরাক </p>
-                        <p className='font-normal'>-----</p>
-                        <p className='font-normal'>-----</p>
-                    </div>
-                    <div className='grid lg:grid-cols-3 border-b-2 border-[#c4c4c4] justify-items-center pb-2'>
-                        <p>সূর্যোদয় </p>
-                        <p className='font-normal'>-----</p>
-                        <p className='font-normal'>-----</p>
-                    </div>
-                    <div className='grid lg:grid-cols-3 border-b-2 border-[#c4c4c4] justify-items-center pb-2'>
-                        <p>সূর্যাস্ত </p>
-                        <p className='font-normal'>-----</p>
-                        <p className='font-normal'>-----</p>
-                    </div>
-                </div>
+        <section className="prayer-section pt-12 pb-7 px-12 border-b-2 border-t-4 border-[#c4c4c4] flex flex-col lg:flex-row justify-center gap-20">
+            <div className='card-child'>
+                {
+                    prayerTime.map(result => <SinglePrayer
+                        key={result._id}
+                        refetch={refetch}
+                        result={result}
+                    ></SinglePrayer>)
+                }
             </div>
-
             <article>
                 <div className='grid grid-cols-2 gap-2 justify-center items-center'>
                     <div className='relative'>
---
+++ src/pages/Home/PrayerTime/SinglePrayer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SinglePrayer = ({ result, refetch }) => {
+    const { name, azan, ekamot } = result;
+    return (
+        <div className="lg:w-96 sm:w-60 bg-[#e9e9e9] shadow-lg px-5">
+            <div className="px-3 text-2xl font-bold grid grid-cols-3 border-[#C2C2C2] border-b-2 py-2">
+                <p>{name}</p>
+                <p className='font-normal'>{azan}</p>
+                <p className='font-normal'>{ekamot}</p>
+            </div>
+        </div>
+    );
+};
+
+export default SinglePrayer;
