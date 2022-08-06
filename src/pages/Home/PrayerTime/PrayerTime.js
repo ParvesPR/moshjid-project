fb054bb379098a49e385a94f70fe2b7baae78d41
ParvesPR <parves2585@gmail.com>
2022-08-06 12:52:20 +0800

    updated prayer time & set responsive for mobile device

--- src/pages/Home/PrayerTime/PrayerTime.js
+++ src/pages/Home/PrayerTime/PrayerTime.js
@@ -18,7 +18,11 @@ const PrayerTime = () => {
     };
     return (
         <section className="prayer-section pt-12 pb-7 px-12 border-b-2 border-t-2 border-[#c4c4c4] flex flex-col lg:flex-row justify-center gap-5">
-            <div className='pl-0 lg:pl-12'>
+            <div className='pl-0 lg:pl-12 order-last lg:order-first'>
+                <div className='flex justify-center gap-4 text-white font-normal text-xl text-center'>
+                    <span className='bg-[#FFA90F] py-1 px-3 rounded-t-md  mx-9 mr-6'>আযান</span>
+                    <span className='bg-[#FFA90F] py-1 px-3 rounded-t-md text-white mx-0'>ইকামত</span>
+                </div>
                 {
                     prayerTime.map(result => <SinglePrayer
                         key={result._id}
@@ -27,30 +31,20 @@ const PrayerTime = () => {
                     ></SinglePrayer>)
                 }
             </div>
-            <article>
-                {/* <div className='flex justify-center items-center'>
-                    <img className='w-full' src={about2} alt="" />
-                    <div className='mt-10 text-4xl lg:text-5xl text-[#FFA810] font-bold text-center'>
-                        <p>আমাদের</p>
-                        <p>সম্পর্কে</p>
-                        <p>
-                            <img src={underline} className='lg:w-5/12 mx-auto' alt="" />
-                        </p>
-                    </div>
-                </div> */}
-                <div class="hero items-start justify-center">
-                    <div class="hero-content flex-row lg:flex-row">
-                        <img src={about2} class="max-w-md w-7/12 lg:w-full" alt='about' />
+            <article className='order-first lg:order-last'>
+                <div className="hero items-start justify-center">
+                    <div className="hero-content flex-row lg:flex-row">
+                        <img src={about2} className="max-w-sm w-7/12 lg:w-full" alt='about' />
                         <div>
-                            <h1 class="text-4xl lg:text-6xl text-[#FFA810] font-bold text-center">আমাদের</h1>
-                            <h1 class="text-4xl lg:text-6xl text-[#FFA810] font-bold text-center">সম্পর্কে</h1>
-                            <img src={underline} className='w-3/4 lg:w-8/12 mx-auto' alt="" />
+                            <h1 className="text-4xl lg:text-7xl text-[#FFA810] font-bold text-center">আমাদের</h1>
+                            <h1 className="text-4xl lg:text-7xl text-[#FFA810] font-bold text-center">সম্পর্কে</h1>
+                            <img src={underline} className='w-3/4 lg:w-11/12 mx-auto' alt="" />
                         </div>
                     </div>
                 </div>
-            <p className='block font-bensen text-xl lg:text-3xl lg:px-8'>
-                মসজিদের উৎকর্ষের ক্ষেত্রে,সেই সপ্তম শতাব্দীর সাদাসিধে খোলা প্রাঙ্গণবিশিষ্ট মসজিদে কাবা বা মসজিদে নববী থেকে বর্তমানে এর প্রভূত উন্নয়ন ঘটেছে। এখন অনেক মসজিদেরই সুবিশাল গম্বুজ,উচু মিনার এবং বৃহদাকার প্রাঙ্গণ দেখা যায়। মসজিদের উৎপত্তি আরব উপদ্বীপে হলেও বর্তমানে তা পৃথিবীর সব দেশেই ছড়িয়ে পড়েছে।
-            </p>
+                <p className='block font-bensen text-xl lg:text-3xl lg:px-8'>
+                    মসজিদের উৎকর্ষের ক্ষেত্রে,সেই সপ্তম শতাব্দীর সাদাসিধে খোলা প্রাঙ্গণবিশিষ্ট মসজিদে কাবা বা মসজিদে নববী থেকে বর্তমানে এর প্রভূত উন্নয়ন ঘটেছে। এখন অনেক মসজিদেরই সুবিশাল গম্বুজ,উচু মিনার এবং বৃহদাকার প্রাঙ্গণ দেখা যায়। মসজিদের উৎপত্তি আরব উপদ্বীপে হলেও বর্তমানে তা পৃথিবীর সব দেশেই ছড়িয়ে পড়েছে।
+                </p>
             </article>
         </section>
     );
--- src/pages/Home/PrayerTime/SinglePrayer.js
+++ src/pages/Home/PrayerTime/SinglePrayer.js
@@ -3,8 +3,8 @@ import React from 'react';
 const SinglePrayer = ({ result, refetch }) => {
     const { name, azan, ekamot } = result;
     return (
-        <div className="lg:w-96 sm:w-60 bg-[#e9e9e9] shadow-lg px-5">
-            <div className="px-3 text-2xl font-bold grid grid-cols-3 border-[#C2C2C2] border-b-[1px] py-3">
+        <div className="lg:w-96 mx-auto bg-[#e9e9e9] shadow-lg px-5">
+            <div className="px-3 text-3xl font-bold grid grid-cols-3 border-[#c2c2c2a5] border-b-[1px] py-2">
                 <p>{name}</p>
                 <p className='font-normal'>{azan}</p>
                 <p className='font-normal'>{ekamot}</p>
